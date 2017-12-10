b0d551a7684e6e407f0dfeaefb37e70d3938b68b
Patrick McLaughlin <patrickmc21@gmail.com>
2017-12-10 13:34:27 -0700

    Input field specification

--- scripts.js
+++ scripts.js
@@ -6,10 +6,16 @@ rightSide.on('click', '.delete-button', deleteBookmark);
 $('.enter-button').on('click', function(){
   var titleInput = $('.title-input');
   var websiteInput = $('.website-input');
-  rightSide.append('<article class=\"card\"> <h2 class=\"card-title\">' + titleInput.val() + '</h2><a href=\"' + websiteInput.val() + '\" class=\"card-url\">' + websiteInput.val() + '</a><button class=\"card-button read-button\">Read</button><button class=\"card-button delete-button\">Delete</button></article>');
+  if (titleInput.val() == '' || websiteInput.val() == '' ) {
+    $('.enter-button').attr('disabled')
+  } else {
+    rightSide.append('<article class=\"card\"> <h2 class=\"card-title\">' + titleInput.val() + '</h2><a href=\"' + websiteInput.val() + '\" class=\"card-url\">' + websiteInput.val() + '</a><button class=\"card-button read-button\">Read</button><button class=\"card-button delete-button\">Delete</button></article>');
   websiteInput.val('');
   titleInput.val('');
+  }
+  
 });
+
 
 
 function deleteBookmark () {
