10949a00a440c34ce532b90fbc5c67af4d16ddc2
Patrick McLaughlin <patrickmc21@gmail.com>
2017-12-09 17:05:16 -0700

    Make enter button work

--- scripts.js
+++ scripts.js
@@ -3,10 +3,8 @@ $('.enter-button').on('click', function(){
   var websiteInput = $('.website-input').val();
   var rightSide = $('.right-side');
   rightSide.append('<article class=\"card\"> <h2 class=\"card-title\">' + titleInput + '</h2><a href=\"' + websiteInput + '\" class=\"card-url\">' + websiteInput + '</a><button class=\"card-button read-button\">Read</button><button class=\"card-button delete-button\">Delete</button></article>');
-  // $('article').on('click', $('.delete')
-  //   $('article').remove()); 
-  console.log(titleInput, websiteInput);
 });
+
 
 
 
