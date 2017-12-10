2667948581a1a90aac51eed5c864b6b5c56b1a42
Emily Kuckelman <ekuckelman@gmail.com>
2017-12-10 14:05:04 -0700

    Refactored functions and got rid of console logs

--- scripts.js
+++ scripts.js
@@ -11,27 +11,21 @@ $('.enter-button').on('click', function(){
   titleInput.val('');
 });
 
-
-function deleteBookmark () {
+function deleteBookmark() {
   $(this).closest('article').remove();
 };
 
-// If, else statement that if the button is clicked, 
-// then toggle the button from disabled to enabled. 
-// Else the button and card css will stay as is. 
-
-var rightSide = $('.right-side');
 rightSide.on('click', '.read-button', markAsRead);
 rightSide.on('dblclick', '.read-button', markAsUnread);
 
 function markAsRead() {
   $(this).closest('.read-button').addClass('read');
   $(this).closest('article').addClass('read-card');
-  console.log('read button clicked');
 }
 
-function markAsUnread () {
+function markAsUnread() {
   $(this).closest('.read-button').removeClass('read');
   $(this).closest('article').removeClass('read-card');
-  console.log('read button double clicked');
 }
+
+
