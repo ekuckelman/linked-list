c9e6a14e66234febe0acaebaf51f00c87d3cd85c
Patrick McLaughlin <patrickmc21@gmail.com>
2017-12-13 14:09:25 -0700

    add url validation to refactor js

--- scripts.js
+++ scripts.js
@@ -9,8 +9,9 @@ rightSide.on('click', '.read-button', markAsRead);
 $('.enter-button').on('click', function(){
   var titleInput = $('.title-input');
   var websiteInput = $('.website-input');
-  if (titleInput.val() == '' || websiteInput.val() == '' ) {
+  if (titleInput.val() === '' || validURL == true ) {
     $('.enter-button').attr('disabled')
+    alert('Please enter a title and valid URL');
   } else {
     rightSide.append('<article class=\"card\"> <h2 class=\"card-title\">' + titleInput.val() + '</h2><a href=\"' + websiteInput.val() + '\" class=\"card-url\">' + websiteInput.val() + '</a><button class=\"card-button read-button\">Read</button><button class=\"card-button delete-button\">Delete</button></article>');
   websiteInput.val('');
@@ -48,3 +49,14 @@ function  linkCounter() {
   var links = $('.card').length;
   $('.count').text(links);
 }
+
+function validURL (){
+  var validateUrl = /^(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-_])+(.[a-z])?/;
+  var regex = new RegExp(validateUrl);
+  var websiteAddress = $('.website-input').val();
+  if (websiteAddress.match(regex)) {
+    return true
+  }else {
+    return false;
+  }
+};
